c922defdc00fc30b9c6f3f8b22981780cc25f214
Aryan-Raj-cs27 <Aryanjaya707@gmail.com>
2026-02-24 00:56:18 +0530

    Added relative path file reading

--- Module_1_Lexical_Analyzer/lexer.java
+++ Module_1_Lexical_Analyzer/lexer.java
@@ -1,7 +1,27 @@
-public class lexer 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Lexer 
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello World");
+        try
+        {
+            File myCodeFile = new File("Module_1_Lexical_Analyzer/input_code.txt");
+            Scanner sc = new Scanner(myCodeFile);
+            System.out.println("--- Reading Input ---");
+            while (sc.hasNextLine()) 
+            {
+                String l = sc.nextLine();
+                System.out.println(l);
+            }
+            sc.close();
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("Input File NOT FOUND.");
+            e.printStackTrace();
+        }
     }
 }
